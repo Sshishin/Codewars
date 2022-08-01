76ab01b9df1ab99a1d0bf5bd0deace1b2a4357a2
Sergey Shishin <flyers32rus@gmail.com>
2022-08-01 21:17:39 +0300

    task #3 - delete ! from string

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log('Hello');
+
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+// Создать функцию которая будет удалять все восклицательные знаки из строки
+
+// function removeExclamationMarks(s) {
+//     return '';
+//   }
+
+// ************ Начал решение *******************
+
+// function removeExclamationMarks(s) {
+//    let y = '';
+//     for(let i of s) {
+//         if (i == '!') {
+//             continue;
+//         } else {
+//              y = y + i
+//         }
+//     }
+//     console.log(y)
+//     return y;
+//   }
+
+// removeExclamationMarks('H!ello! World!')
+
+// ************ РЕШЕНО *******************
+
+
+// Решения которые мне понравились
+
+// function removeExclamationMarks(s) {
+//     return s.split('!').join('');
+//   }
+
+  
