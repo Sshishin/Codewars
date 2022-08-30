081b809baffb0641d61043c2ef042059a7c88f5b
Sergey Shishin <flyers32rus@gmail.com>
2022-08-30 16:43:55 +0300

    reverse string and binary numbers

--- script.js
+++ script.js
@@ -85,20 +85,106 @@
 
 // Задача с переводом зачений к 1 или 0
 
-function fakeBin(x){
-  let str = '';
+// function fakeBin(x){
+//   let str = '';
 
-  for(let i = 0; i < x.length; i++) {
-  if(x[i] >= 0 && x[i] < 5){
-   str += 0;
-  }
+//   for(let i = 0; i < x.length; i++) {
+//   if(x[i] >= 0 && x[i] < 5){
+//    str += 0;
+//   }
 
-  if(x[i] >= 5) {
-    str += '1';
+//   if(x[i] >= 5) {
+//     str += '1';
+//   }
+// }
+//   return str;
+
+// }
+
+// console.log(fakeBin('3219893195'));
+
+
+// Counting sheep
+// Самостоятельное решение
+
+//  const arr = [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true];
+
+
+
+// function countSheeps(arrayOfSheep) {
+//   let total = 0;
+//   arrayOfSheep.forEach((count) => {
+//     if(count) {
+//       total++;
+//     }
+//   });
+//   return total;
+// }
+
+// console.log(countSheeps(arr));
+
+// //  multiplying two numbers
+// // Самостоятельное решение
+
+// const multiply = (a, b) => a * b;
+
+// console.log(multiply(3, 4));
+
+
+// Перевод в бинарное
+// Самостоятельное решение
+
+// function addBinary(a,b) {
+//  return Number(a + b).toString(2);
+// }
+
+// console.log(addBinary(2,3));
+
+
+//Мое решение
+
+function getGrade (s1, s2, s3) {
+  const average = (s1 + s2 + s3) / 3;
+  if(average <= 100 && average >= 90) {
+    return 'A';
+  } else if (average <= 90 && average >= 80) {
+    return 'B';
+  } else if (average <= 80 && average >= 70) {
+    return 'C';
+  } else if (average <= 70 && average >= 60) {
+    return 'D';
+  } else if (average <= 60) {
+    return 'F';
   }
 }
-  return str;
+console.log(getGrade (100, 39, 89));
 
+
+// Решение которое мне порнравилось
+
+function getGrade (s1, s2, s3) {
+  var s = (s1 + s2 + s3) / 3;
+  return s >= 90 ? "A" : s >= 80 ? "B" : s >= 70 ? "C" : s >= 60 ? "D" : "F"
 }
 
-console.log(fakeBin('3219893195'));
+// Переворот строки
+// Самостоятельное решение
+
+function reverseWords(str) {
+  const splitString = str.split(' ');
+  let sumString = '';
+
+  for(let i = 0; i <= splitString.length - 1; i++) {
+    if(i == splitString.length - 1) {
+      sumString += splitString[i].split('').reverse().join('');
+    } else {
+      sumString += splitString[i].split('').reverse().join('') + ' ';
+    }
+  }
+  return sumString;
+}
